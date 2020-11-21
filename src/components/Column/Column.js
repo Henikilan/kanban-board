1197feb78ec6d252284a4959ee1069621cee6cff
henikilan <henikilan@mail.ru>
2020-11-21 15:25:30 -0500

    move card functionality

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,22 +3,31 @@ import "./Column.css";
 import Issues from "../Issues/Issues";
 import Button from "../Button/Button";
 import AddCardForm from "../AddCard/AddCardForm";
+import IssueSelector from "../IssueSelector/IssueSelector";
 
-const Column = ({ title, issues, addBacklogCard }) => {
+const Column = ({ title, issues, addBacklogCard, previousIssues, onMove }) => {
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const isBacklog = title === "Backlog";
   const handleSubmit = (name) => {
-    if (title === "Backlog") {
+    if (isBacklog) {
       addBacklogCard(name);
     }
     setIsFormOpen(false);
   };
-
+  const handleSelect = (id) => {
+    onMove(id);
+    setIsFormOpen(false);
+  };
   return (
     <div className="column">
       <div className="title">{title}</div>
       <Issues issues={issues} />
       {isFormOpen ? (
-        <AddCardForm onSubmit={handleSubmit} />
+        isBacklog ? (
+          <AddCardForm onSubmit={handleSubmit} />
+        ) : (
+          <IssueSelector issues={previousIssues} onSelect={handleSelect} />
+        )
       ) : (
         <Button onClick={() => setIsFormOpen(true)} />
       )}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -22,15 +22,38 @@ const Main = ({ data, setData }) => {
       ),
     ]);
   };
-  const moveCard = () => {};
+  const moveCard = (id, index) => {
+    setData([
+      ...data.map((dataItem, currentIndex) => {
+        if (currentIndex === index) {
+          return {
+            ...dataItem,
+            issues: [
+              ...dataItem.issues,
+              data[index - 1].issues.find((issue) => issue.id === id),
+            ],
+          };
+        }
+        if (currentIndex === index - 1) {
+          return {
+            ...dataItem,
+            issues: dataItem.issues.filter((issue) => issue.id !== id),
+          };
+        }
+        return dataItem;
+      }),
+    ]);
+  };
   return (
     <main className="main">
-      {data.map(({ title, issues }) => (
+      {data.map(({ title, issues }, index) => (
         <Column
           key={title}
           title={title}
           issues={issues}
+          previousIssues={data[index - 1] ? data[index - 1].issues : []}
           addBacklogCard={addBacklogCard}
+          onMove={(id) => moveCard(id, index)}
         />
       ))}
     </main>
